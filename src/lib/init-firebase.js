906eadab0c87fde3df4770d3756715ecdaf6fe23
Blueshadow58 <gamonalfranco@gmail.com>
2022-08-22 01:27:33 -0400

    env Config

--- src/lib/init-firebase.js
+++ src/lib/init-firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_projectId}`,
-  storageBucket: `${process.env.REACT_APP_storageBucket}`,
-  messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`,
-  appId: `${process.env.REACT_APP_APP_ID}`,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
